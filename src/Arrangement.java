40f0e822eae2005fead298a942065d63f8df5a83
井鱼 <1538109928@qq.com>
2020-10-12 11:48:23 +0800

    括号的排列组合

---
+++ src/Arrangement.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Arrangement {
+
+    /**
+     * 求出括号的排列组合
+     * @param num
+     * @return allList
+     */
+    public int getArrangeNum(int num) {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
